74e00cbc7dcee57c05169c9ca8c38174c323226c
MrEpoch <alexandrstencuk@seznam.cz>
2023-06-30 16:10:28 +0200

    Init of git on github

---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+import "./header.css";
+
+export function Header() {
+    return (
+        <header className="shop-header">
+            <div className="header__logo">
+
+            </div>
+            <div className="header__navigation">
+
+            </div>
+            <div className="header__controls">
+                <div className="header__search__container">
+                    <input type="text" className="header__search__input" placeholder="Search" />
+                </div>
+                <div className="header__user__container">
+                    
+                </div>
+            </div>
+        </header>
+    )
+}
